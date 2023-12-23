788c304cd4477d58136314b1afe39ffed1f39c78
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-23 12:58:21 +0530

    WeatherApplication

---
+++ Weather_Application/index.js
@@ -0,0 +1,33 @@
+// Get the current date
+const currentDate = new Date();
+
+// Format the date as desired
+const formattedDate = currentDate.toDateString();
+
+// Update the date section in your HTML
+const dateSection = document.getElementById('Date');
+dateSection.textContent = formattedDate;
+
+// Get the current city
+const currentCity = "New York"; // Replace with your logic to get the current city
+
+// Update the location section in your HTML
+const locationSection = document.getElementById('location');
+locationSection.textContent = `${currentCity}`;
+
+// Get the temperature from the API
+fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}' + currentCity)
+    .then(response => response.json())
+    .then(data => {
+        const currentTemperature = data.current.temp_c;
+
+        // Update the temperature section in your HTML
+        const temperatureSection = document.getElementById('temperature');
+        temperatureSection.textContent = `${currentTemperature}°C`;
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+
+
