c8f674e97d6d326919517a95c8633a286e188b24
Shekhar <115777486+shekhardase@users.noreply.github.com>
2023-12-24 16:55:42 +0530

    Weather App

--- Weather_Application/index.js
+++ Weather_Application/index.js
@@ -1,33 +1,36 @@
-// Get the current date
-const currentDate = new Date();
+const apiKey = '514c2805397f8ff97146a220faede581';
+const apiUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
-// Format the date as desired
-const formattedDate = currentDate.toDateString();
-
-// Update the date section in your HTML
-const dateSection = document.getElementById('Date');
-dateSection.textContent = formattedDate;
-
-// Get the current city
-const currentCity = "New York"; // Replace with your logic to get the current city
-
-// Update the location section in your HTML
-const locationSection = document.getElementById('location');
-locationSection.textContent = `${currentCity}`;
-
-// Get the temperature from the API
-fetch('https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={514c2805397f8ff97146a220faede581}' + currentCity)
-    .then(response => response.json())
-    .then(data => {
-        const currentTemperature = data.current.temp_c;
-
-        // Update the temperature section in your HTML
-        const temperatureSection = document.getElementById('temperature');
-        temperatureSection.textContent = `${currentTemperature}°C`;
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+const locationInput = document.getElementById('locationInput');
+const searchButton = document.getElementById('searchButton');
+const locationElement = document.getElementById('location');
+const temperatureElement = document.getElementById('temperature');
+const descriptionElement = document.getElementById('description');
+const dateElement = document.getElementById('Date');
 
 
+searchButton.addEventListener('click', () => {
+    const location = locationInput.value;
+    if (location) {
+        fetchWeather(location);
+    }
 
+});
+
+function fetchWeather(location) {
+    const url = `${apiUrl}?q=${location}&appid=${apiKey}&units=metric`;
+
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            locationElement.textContent = data.name;
+            temperatureElement.textContent = `${Math.round(data.main.temp)}°C`;
+            descriptionElement.textContent = data.weather[0].description;
+            dateElement.textContent = new Date().toLocaleDateString();
+
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+}   
